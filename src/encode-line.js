895c36b1f64bdcb815bf73a109739bf8e6b4cf81
t_frrolova <tatianaaleks.frolova@gmail.com>
2023-04-12 23:34:05 +0200

    6 tasks finished

--- src/encode-line.js
+++ src/encode-line.js
@@ -11,7 +11,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function encodeLine(str) {
-  
+  if (str.length === 0) {
+    return '';
+  }
+
+  let lastLetter = '';
+  let counter = 1;
+  let result = '';
+
+  for (let i = 0; i < str.length; i++) {
+
+    if (str[i] === lastLetter) {
+      counter = counter + 1;
+      if (i === str.length - 1) {
+        result = result + (counter > 1 ? counter : '') + lastLetter;
+      }
+    } else {
+      if ((i === str.length - 1)) {
+        result = result + (counter > 1 ? counter : '') + lastLetter + str[i];
+      } else {
+      result = result + (counter > 1 ? counter : '') + lastLetter;
+      lastLetter = str[i];
+      counter = 1;
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = {
