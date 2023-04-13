485ad2d54237692b4847bcc88e4e4bd798dd22d9
t_frrolova <tatianaaleks.frolova@gmail.com>
2023-04-13 22:37:07 +0200

    email

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -14,8 +14,8 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 class DepthCalculator {
   calculateDepth(arr) {
-    if(arr.filter(i => i.constructor.name === "Array").length != 0){
-      return 1 + this.calculateDepth([].concat(...arr.filter(i => i.constructor.name === "Array")));
+    if(arr.filter(item => item.constructor.name === "Array").length != 0){
+      return 1 + this.calculateDepth([].concat(...arr.filter(item => item.constructor.name === "Array")));
     } else {
       return 1;
     }
