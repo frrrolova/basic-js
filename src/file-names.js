f49e977ec38fad2e4d2cfd16e3445a4cf839bf72
t_frrolova <tatianaaleks.frolova@gmail.com>
2023-12-14 21:41:25 +0100

    feat: minesweeper and file names are done

--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  const newArr = [];
+  for (let i = 0; i < names.length; i += 1) {
+    if (!newArr.includes(names[i])) {
+      newArr.push(names[i]);
+    } else {
+      for (let j = 0; j < newArr.length; j++) {
+        const newFile = `${names[i]}(${j + 1})`
+        if (!newArr.includes(newFile)) {
+            newArr.push(newFile)
+            break
+        }
+      }
+    }
+  }
+  // const map = new Map();
+  // const result = [];
+  // names.forEach((item) => {
+  //   if (map.has(item)) {
+  //     map.set(item, map.get(item) + 1);
+  //   } else {
+  //     map.set(item, 0);
+  //   }
+  // })
+  // map.forEach((key, val) => {
+    
+  // })
+  return newArr;
 }
 
 module.exports = {
--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let rows = matrix.length;
+  let cols = matrix[0].length;
+  const result = [];
+  for (let i = 0; i < rows; i += 1) {
+    const resultRow = [];
+    for (let j = 0; j < cols; j += 1) {
+      let num = 0;
+      for (let x = -1; x <= 1; x++) {
+        for (let y = -1; y <= 1; y++) {
+          let rowInd = i + y;
+          let colInd = j + x;
+          if (rowInd === i && colInd === j) {
+            continue;
+          }
+          let checkingVal;
+          if (matrix[rowInd] !== undefined && matrix[rowInd][colInd] !== undefined) {
+            checkingVal = matrix[rowInd][colInd];
+          }
+          if (checkingVal) {
+            num += 1;
+          }
+        }
+      }
+      resultRow.push(num);
+    }
+    result.push(resultRow);
+  }
+  console.debug(result);
+  return result;
 }
 
 module.exports = {
